fdf6193f766dc2e59ddb253d7aa8f78c61fcb958
Julian Bellafronte <julianbellafronte1999@gmail.com>
2022-01-24 00:05:58 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+gsap.registerPlugin(ScrollTrigger)
+
+let container = document.getElementById("container")
+
+gsap.to(container, {
+  x: () => -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+  ease: "none",
+  scrollTrigger: {
+    trigger: container,
+    invalidateOnRefresh: true,
+    pin: true,
+    scrub: 0.5,
+    end: () => "+=" + container.offsetWidth
+  }
+})
